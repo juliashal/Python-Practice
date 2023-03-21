2e89b8575564cbf3ac4bee40982d2f228a01598a
Iuliia Shal <julia.d.shal@gmail.com>
2023-03-22 11:31:00 +1300

    Leave only functions to check and apply the player's turn

--- TicTacToe_Fill_the_board.py
+++ TicTacToe_Fill_the_board.py
@@ -5,48 +5,35 @@ game = [[0, 0, X],
 	    [0, 0, 0]]
 And ask Player 2 for their move, printing an O in that place.'''
 
-game = [[0, 0, 0],
+"""game = [[0, 0, 0],
         [0, 0, 0],
         [0, 0, 0]]
 
-players = {'Player 1': 'X', 'Player 2': 'O'}
+players = {'Player 1': 'X', 'Player 2': 'O'}"""
 
 
-def check_turn():
+def check_turn(size):
 
     while True:
         turn = input("Type your move (format: row number,column number): ")
-        # digits count
-        digits = [x for x in turn if x.isdigit() and int(x) < 4 and int(x) > 0]
+        # digits count and max position checks
+        digits = [x for x in turn if x.isdigit() and int(x) <= size and int(x) > 0]
         if len(digits) == 2 and ("," in turn):
             break
 
     turn = list(turn)
     turn.remove(",")
+    '''create indexes pair'''
     turn = [int(x)-1 for x in turn]
     return turn
 
 
-def turn(player):
-    turn = check_turn()
+def turn(game, size, player):
+    turn = check_turn(size)
 
     while game[turn[0]][turn[1]] != 0:
         print("This cell is occupied")
         turn = check_turn()
 
     game[turn[0]][turn[1]] = player
-    print(game)
-
-
-spaces_left = sum([sublist.count(0) for sublist in game])
-
-while spaces_left > 0:
-    for player in players:
-        print(f"it's {player} turn")
-        turn(players[player])
-        spaces_left -= 1
-        if spaces_left == 0:
-            print(f"End of the game")
-            break
-
-    spaces_left
+    return game
