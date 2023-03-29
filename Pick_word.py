f5bcdd12178acfdcc4c70c06e54dbf06f5d60c82
Iuliia Shal <julia.d.shal@gmail.com>
2023-03-29 19:29:07 +1300

    Adj to use function in Hagman file

--- Pick_word.py
+++ Pick_word.py
@@ -1,7 +1,10 @@
 '''In this exercise, the task is to write a function that picks a random word from a list of words from the SOWPODS dictionary. 
 Download this file and save it in the same directory as your Python code. '''
 
-#Download file wth words
+
+'''>>> Run the code below to download list of words
+
+#Download file with words
 
 from urllib.request import urlopen
 import random
@@ -18,10 +21,15 @@ with urlopen(url) as file:
 with open(save_as, 'w') as download:
     download.write(content)
 
+>>> '''
+
+from urllib.request import urlopen
+import random
+
 def random_word():
     with open("sowpods.txt","r") as file:
         file_lines = file.readlines()
         random_index = random.randint(1, len(file_lines))
-        print(f"Random index is {random_index}, Random word is {file_lines[random_index]}")
+        #print(f"Random index is {random_index}, Random word is {file_lines[random_index]}")
 
-random_word()
+    return file_lines[random_index].strip()
